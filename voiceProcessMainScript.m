17c60d2fdc353f734ba1acf9d4dbc243fd516467
ljl86400 <510758767@qq.com>
2019-11-09 14:00:36 +0800

    add data recover code & some post process code

--- voiceProcessMainScript.m
+++ voiceProcessMainScript.m
@@ -12,6 +12,9 @@ soundCardBufferDataFramesLen = 480;
 if(size(rowData,2) >= 2)
     rowData = sum(rowData,2)/size(rowData,2);
 end
+numOfSample = length(rowData);
+tRowData = numOfSample/fs;
+rowTimeAxis = 1/fs:1/fs:tRowData;
 
 % frame
 soundCardBufferData = buffer(rowData,soundCardBufferDataFramesLen,0,'nodelay');
@@ -22,6 +25,7 @@ rateOfProcess = 1;
 rebufferData = zeros(320,1);
 win = hamming(320);
 fsProcess = 16000;
+recoverData = zeros(160,1);
 showWaitBar = waitbar(rateOfProcess,'rate of process');
 while(rateOfProcess <= soundCardBufferDataFramesNum)
     
@@ -47,10 +51,28 @@ while(rateOfProcess <= soundCardBufferDataFramesNum)
     % acoustic echo cancellation
     % agc
     
+    %% data recover
+    curTrunkProcessResult = detrendData;
+    recoverData = [recoverData(1:end - 160);recoverData(end - 159:end) + curTrunkProcessResult(1:end/2)...
+        ;curTrunkProcessResult(end/2 +1:end)];
     waitbar(rateOfProcess/soundCardBufferDataFramesNum);
     rateOfProcess = rateOfProcess + 1;
 end
 close(showWaitBar);
 
 %% output data post processing
+figure('name','process result')
+hold on 
+plot(rowTimeAxis,rowData)
+tProcess = length(recoverData)/fsProcess;
+processTimeAxis = 1/fsProcess:1/fsProcess:tProcess;
+plot(processTimeAxis,recoverData)
+hold off
+
+sound(rowData,fs)
+pause(tRowData)
+sound(recoverData,fsProcess)
+pause(tProcess)
+
+
 
